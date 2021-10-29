af3c54aa6a4715df116ed100656df1a5a48f0402
SuhailAshraf <suhailashraf019@gmail.com>
2021-10-30 00:35:44 +0530

    smail Hamburger application created

---
+++ Smail/app/src/main/java/com/suhailashraf/smail/ui/Sent.java
@@ -0,0 +1,38 @@
+package com.suhailashraf.smail.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.suhailashraf.smail.R;
+
+public class Sent extends Fragment {
+
+    private SentViewModel mViewModel;
+
+    public static Sent newInstance() {
+        return new Sent();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sent_fragment3, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SentViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Smail/app/src/main/java/com/suhailashraf/smail/ui/SentViewModel.java
@@ -0,0 +1,7 @@
+package com.suhailashraf.smail.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class SentViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
